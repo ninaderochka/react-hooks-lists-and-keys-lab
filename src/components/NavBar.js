3913b7fd82b787a157cf47ae34b1330c57e02344
Nina Hawari <nina.hawari@gmail.com>
2022-12-09 23:52:00 +0200

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
 import React from "react";
+import home from "./Home"
 
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav><a key={home.id} href="#home">home</a>
+  <a href="#about">about</a>
+  <a href="#projects">projects</a></nav>;
 }
 
 export default NavBar;
